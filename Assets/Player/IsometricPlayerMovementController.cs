b81bae4c0073e2e2ace1ce981fdd6e8b66bfe1ca
Callum Veale <variableclass@users.noreply.github.com>
2020-02-01 16:59:51 +0100

    Fixed sprite glitching and restricted to intercardinal movement only

--- Assets/Player/IsometricPlayerMovementController.cs
+++ Assets/Player/IsometricPlayerMovementController.cs
@@ -1,89 +1,157 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class IsometricPlayerMovementController : MonoBehaviour
 {
+    #region Enums
 
-    public float movementSpeed = 1.5f;
-    public Camera myCamera;
-    //IsometricCharacterRenderer isoRenderer;
-    SpriteRenderer sprite;
-    public GameObject gridObject;
-    public Sprite NE;
-    public Sprite NW;
-    public Sprite SE;
-    public Sprite SW;
+    private enum Bearing
+    {
+        NorthEast,
+        SouthEast,
+        SouthWest,
+        NorthWest
+    }
 
-    Rigidbody2D rbody;
+    #endregion
+
+    #region Properties
+
+    public float PlayerSpeed = 1.5f;
+    public Camera Camera;
+    public Sprite NorthEast;
+    public Sprite NorthWest;
+    public Sprite SouthEast;
+    public Sprite SouthWest;
+
+    #endregion
+
+    #region Member Variables
+
+    private SpriteRenderer spriteRenderer;
+    private GameObject grid;
+
+    #endregion
+
+    #region Methods
 
     private void Awake()
     {
-        rbody = GetComponent<Rigidbody2D>();
-        //isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
-        sprite = GetComponent<SpriteRenderer>();
-        gridObject = GameObject.FindGameObjectWithTag("Grid"); 
-        if (!gridObject)
+        // Get game object components
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Get grid object
+        grid = GameObject.FindGameObjectWithTag("Grid"); 
+        if (!grid)
         {
-            print("Cannot find Grid."); 
+            throw new Exception("No grid found");
         }
     }
 
-
-    Vector2 FindDirection(Vector2 mousePosition, Vector2 characterPosition)
+    private Vector2? GetHeading(Vector3 mousePosition, Vector3 characterPosition)
     {
-        Vector2 moveDirection;
+        // If the character is at the cursor position, do not move
+        if ((characterPosition.x == mousePosition.x) || (characterPosition.y == mousePosition.y))
+        {
+            return null;
+        }
 
-        //Check if x and y direction compared to mouse is negative or positive
-        moveDirection.x = characterPosition.x < mousePosition.x ? 1 : -1;
-        moveDirection.y = characterPosition.y < mousePosition.y ? 1 : -1;
+        var x = mousePosition.x - characterPosition.x;
+        var y = mousePosition.y - characterPosition.y;
 
-
-        return moveDirection;
+        return new Vector2(x, y);
     }
 
-    void SetSprite(Vector2 moveDirection)
+    private Vector2? GetIsometricHeading(Vector2 heading)
     {
-        //Both positive: NE
-        if (moveDirection.x > 0 && moveDirection.y > 0)
+        var margin = 0.05;
+
+        if ((heading.x < margin && heading.x > -margin) || (heading.y < margin && heading.y > -margin))
         {
-            sprite.sprite = NE;
+            return null;
         }
 
-        //Y positive, X negative: NW
-        else if (moveDirection.x < 0 && moveDirection.y > 0)
-        {
-            sprite.sprite = NW;
-        }
+        var x = heading.x > 0 ? 1 : -1;
+        var y = heading.y > 0 ? 1 : -1;
 
-        //Both negative: SW
-        else if (moveDirection.x < 0 && moveDirection.y < 0)
-        {
-            sprite.sprite = SW;
-        }
+        return new Vector2(x, y);
+    }
 
+    private Bearing GetBearing(Vector2 heading)
+    {
+        if (heading.x > 0 && heading.y > 0)
+        {
+            // Both positive: NE
+            return Bearing.NorthEast;
+        }
+        else if (heading.x < 0 && heading.y > 0)
+        {
+            // Y positive, X negative: NW
+            return Bearing.NorthWest;
+        }
+        else if (heading.x < 0 && heading.y < 0)
+        {
+            // Both negative: SW
+            return Bearing.SouthWest;
+        }
         else
         {
-            sprite.sprite = SE;
+            return Bearing.SouthEast;
         }
+    }
 
+    private Sprite GetSpriteForBearing(Bearing bearing)
+    {
+        if (bearing == Bearing.NorthEast)
+        {
+            return NorthEast;
+        }
+        else if (bearing == Bearing.NorthWest)
+        {
+            return NorthWest;
+        }
+        else if (bearing == Bearing.SouthEast)
+        {
+            return SouthEast;
+        }
+        else
+        {
+            return SouthWest;
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetMouseButton(0)) {
-            //Get mouse position
-            Vector3 mousePosition = myCamera.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButton(0))
+        {
+            // Get mouse position
+            var mousePosition = Camera.ScreenToWorldPoint(Input.mousePosition);
 
-            //Check which diagonal the player is going in
-            var moveVector = FindDirection(mousePosition, transform.position);
+            // Determine the character heading from the character's current position and the mouse position
+            var heading = GetHeading(mousePosition, transform.position);
 
+            // Get an isometric heading for the given heading
+            var isometricHeading = GetIsometricHeading(heading.Value);
 
-            SetSprite(moveVector);
+            // If the character is at the cursor position, do not move
+            if (!isometricHeading.HasValue)
+            {
+                return;
+            }
 
-            //Execute movement
-            transform.Translate(moveVector * Time.deltaTime * movementSpeed);
+            // Get the compass bearing for the heading
+            var bearing = GetBearing(heading.Value);
+
+            // Set the appropriate character sprite for the given bearing
+            spriteRenderer.sprite = GetSpriteForBearing(bearing);
+
+            // Move the character
+            transform.Translate(isometricHeading.Value * Time.fixedDeltaTime * PlayerSpeed);
         }
     }
+
+    #endregion
 }
