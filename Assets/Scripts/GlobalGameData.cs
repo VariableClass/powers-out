dbc2ba0d79f581758a118b7bfb87f66c5b274def
Kaya Masterød Karlsen <kaya.karlsen@communicate.no>
2020-02-01 12:55:22 +0100

    Global data script holding data between scenes

---
+++ Assets/Scripts/GlobalGameData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalGameData : MonoBehaviour
+{
+    public bool keyGotten;
+    public Vector3 playerPos;
+    public int snakeHighScore;
+    public bool radioFixed;
+    public bool powerFixed;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        keyGotten = false;
+        snakeHighScore = 0;
+        radioFixed = false;
+        powerFixed = false;
+
+        print("init global data");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TEST.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TEST : MonoBehaviour
+{
+    public GameObject globalData;
+
+    private void Awake()
+    {
+        globalData = GameObject.FindGameObjectWithTag("GlobalData");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        globalData.GetComponent<GlobalGameData>().powerFixed = true;
+        StartCoroutine(ExampleCoroutine());
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+
+        SceneManager.LoadScene(1);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
