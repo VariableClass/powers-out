24405d7e0b62938779964ed503eb4a69f995f0cc
June V. E. Hansen <jvhansen@stud.ntnu.no>
2020-02-02 01:56:47 +0100

    Added Radio minigame

---
+++ Assets/ChangeText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeText : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    private GlobalGameData globalGameData;
+
+    public Sprite HaveBatteries;
+    public Sprite NoBatteries;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get global game data
+        var globalGameDataGameObject = GameObject.FindGameObjectWithTag("GlobalData");
+        globalGameData = globalGameDataGameObject.GetComponent<GlobalGameData>();
+
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (globalGameData.batteryCount > 0)
+        {
+            sprite.sprite = HaveBatteries;
+        }
+    }
+}
