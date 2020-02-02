70926a03f8d6e98cbdee4894bded044aceb410b9
Kaya Masterød Karlsen <kaya.karlsen@communicate.no>
2020-02-02 13:28:31 +0100

    MY OWN GIT almost

---
+++ Assets/Scripts/BatteryPickup.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPickup : MonoBehaviour
+{
+    public GameObject interactText;
+    public bool canInteract;
+    public GlobalGameData globalData;
+    public GameObject player;
+    public int batteryId;
+
+    private void Awake()
+    {
+        globalData = GameObject.FindGameObjectWithTag("GlobalData").GetComponent<GlobalGameData>();
+    }
+
+    void Start()
+    {
+        canInteract = false;
+        if (globalData.batteryCount.IndexOf(batteryId) != -1)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (canInteract && Input.GetKey(KeyCode.E))
+        {
+            globalData.batteryCount.Add(batteryId);
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            canInteract = true;
+            interactText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            canInteract = false;
+            interactText.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/RadioFix.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioFix : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    private GlobalGameData globalGameData;
+
+    public Sprite RadioNoBattery;
+    public Sprite RadioWithBattery;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get global game data
+        var globalGameDataGameObject = GameObject.FindGameObjectWithTag("GlobalData");
+        globalGameData = globalGameDataGameObject.GetComponent<GlobalGameData>();
+
+        sprite = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (globalGameData.batteryCount.Count > 0)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                sprite.sprite = RadioWithBattery;
+            }
+        }
+
+
+        //TODO: insert battery visually
+        //Good job message
+        //Change music
+    }
+}
